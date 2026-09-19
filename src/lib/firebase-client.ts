bb2a7dc2ca481b11b04938750c809edc175f1403
Ulrik-Matemu <advancechronicles@gmail.com>
2026-09-19 17:12:49 +0300

    deploying portal #2

--- src/lib/firebase-client.ts
+++ src/lib/firebase-client.ts
@@ -2,8 +2,8 @@ import { getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_FIREBASE_AUTH_DOMAIN,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
 };
 
